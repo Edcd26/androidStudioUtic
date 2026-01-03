b666ef0a34e2bf05547560c8f9e54fb6f216e2a8
Edcd26 <elviodenis26@gmail.com>
2026-01-03 00:37:21 -0300

    "se agregaron las funciones de usuario"

--- app/src/main/java/com/example/prueba/UsuarioActivity.java
+++ app/src/main/java/com/example/prueba/UsuarioActivity.java
@@ -17,9 +17,6 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class UsuarioActivity extends AppCompatActivity {
     private Cursor fila;
